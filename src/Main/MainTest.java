84418f27baefa0396163c6569930ed7830f4fefe
jcamilovallejos <jcamilovallejos@gmail.com>
2022-04-19 22:18:58 -0500

    Tarea 007: para parking car aumento de (startdate, enddate, hours parking y type parking) Renombrar el metodo createParkingCar por costParkingCar ya que este test calcula el costo
    para motorcycle adicion de test para(cost,save y list)

--- src/Main/MainTest.java
+++ src/Main/MainTest.java
@@ -10,11 +10,12 @@ import Parking.ParkingCar;
 import Parking.ParkingMotorcycle;
 import Parking.ParkingTruck;
 import Service.ParkingService;
+import Parking.Enum.TypeVehicle;
 
 class MainTest {
 
 	@Test
-	void createParkingCar() {
+	void costParkingCar() {
 		ParkingCar parkingCar = new ParkingCar("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
 				Timestamp.valueOf("2018-09-01 11:15:00"));
 		assertEquals(parkingCar.getCostTotal(), (Double)5000.0);
@@ -40,6 +41,77 @@ class MainTest {
 		Boolean saveParkingCar2 = service.save(parkingCar2);
 		List<Parking> lista = service.listar();
 		assertEquals(lista.size(), 2);
+	}
+	
+
+	@Test
+	void startDateParkingCar() {
+		ParkingCar parkingCar = new ParkingCar("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		String dateTime = "2018-09-01 09:00:00";
+		Timestamp timestamp = Timestamp.valueOf(dateTime);
+		assertEquals(parkingCar.getStartDate(), timestamp);
+	}
+	
+	@Test
+	void endDateParkingCar() {
+		ParkingCar parkingCar = new ParkingCar("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		String dateTime = "2018-09-01 11:15:00";
+		Timestamp timestamp = Timestamp.valueOf(dateTime);
+		assertEquals(parkingCar.getEndDate(), timestamp);
+	}
+	
+	@Test
+	void hoursParkingCar() {
+		ParkingCar parkingCar = new ParkingCar("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		assertEquals(parkingCar.getParkingHours(), (Long)2L);
+	}
+	
+	
+	@Test
+	void typeParkingCar() {
+		ParkingCar parkingCar = new ParkingCar("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-08-01 11:15:00"));
+		assertEquals(parkingCar.getVehicle(), TypeVehicle.CARRO);
+	}
+	
+	
+	@Test
+	void parkingMotorcycle() {
+		ParkingMotorcycle parkingMotorcycle = new ParkingMotorcycle("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		assertEquals(parkingMotorcycle.getCostTotal(), (Double)3000.0);
+	}
+	
+	@Test
+	void saveParkingMotorcycle() {
+		ParkingService service = new ParkingService();
+		ParkingMotorcycle parkingMotorcycle = new ParkingMotorcycle("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		Boolean saveParkingMotorcycle = service.save(parkingMotorcycle);
+		assertEquals(saveParkingMotorcycle, true);
+	}
+	
+	
+	@Test
+	void listParkingMotorcycle() {
+		ParkingService service = new ParkingService();
+		ParkingMotorcycle parkingMotorcycle = new ParkingMotorcycle("code_001", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		Boolean saveParkingCar = service.save(parkingMotorcycle);
+	
+		ParkingMotorcycle parkingMotorcycle2 = new ParkingMotorcycle("code_002", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		Boolean saveParkingCar2 = service.save(parkingMotorcycle2);
+		
+		ParkingMotorcycle parkingMotorcycle3 = new ParkingMotorcycle("code_003", Timestamp.valueOf("2018-09-01 09:00:00"),
+				Timestamp.valueOf("2018-09-01 11:15:00"));
+		Boolean saveParkingCar3 = service.save(parkingMotorcycle3);
+		
+		List<Parking> lista = service.listar();
+		assertEquals(lista.size(), 3);
 	}
 	
 }
